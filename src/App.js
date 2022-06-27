8e55a169beda9a5cb52bda4e8517539d87e69a33
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-27 18:05:47 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import Footer from "./Layout/Footer/Footer";
 import Header from "./Layout/Header/Header";
 import Index from "./Pages/Home/Index";
-
+import { Route, Routes } from "react-router-dom";
+import Mint from "./Pages/Mint/Mint";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Index />
+      <Routes>
+        <Route path={"/"} element={<Index />} />
+        <Route path={"/Mint"} element={<Mint />} />
+      </Routes>
+
       <Footer />
     </div>
   );
---
+++ src/Pages/Mint/Mint.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Mint.css";
+function Mint() {
+  return <div className="mint">Mint</div>;
+}
+
+export default Mint;
